50d6957b290014cd650629ca4ee55e42620c2c6b
Nikita Naleikin <trubatvoia3@gmail.com>
2021-03-21 18:39:47 +0400

    Improves today details block

--- src/components/TodayDetails/TodayDetailsList.vue
+++ src/components/TodayDetails/TodayDetailsList.vue
@@ -1,16 +1,38 @@
 <script>
+const icons = require.context('./icons');
+
 export default {
   name: 'TodayDetailsList',
+  data() {
+    return {
+      listItems: [
+        { name: 'High / Low', icon: 'temperature', value: '12° / 6°' },
+        { name: 'Wind', icon: 'wind', value: '5.5 m/h' },
+        { name: 'Pressure', icon: 'pressure', value: '1029 mbar' },
+        { name: 'Visibility', icon: 'visibility', value: '10 miles' },
+        { name: 'Humidity', icon: 'humidity', value: '61%' },
+      ],
+    };
+  },
+  methods: {
+    getIcon(name) {
+      try {
+        return icons(`./${name}.svg`);
+      } catch (err) {
+        return undefined;
+      }
+    },
+  },
 };
 </script>
 
 <template>
   <ul>
-    <li class="temperature">High / Low</li>
-    <li class="wind">Wind</li>
-    <li class="pressure">Pressure</li>
-    <li class="visibility">Visibility</li>
-    <li class="humidity">Humidity</li>
+    <li v-for="item in listItems" :key="item.name">
+      <img :src="getIcon(item.icon)" alt="" />
+      {{ item.name }}
+      <span>{{ item.value }}</span>
+    </li>
   </ul>
 </template>
 
@@ -27,34 +49,18 @@ li {
   padding: 10px 30px 10px 60px;
   &:last-child {
     padding-bottom: 0;
-    &:before {
+    img {
       top: calc(50% + 10px);
       transform: translateY(calc(-50% - 5px));
     }
   }
-  &:before {
-    width: 25px;
-    height: 25px;
-    margin-right: 10px;
-    position: absolute;
-    left: 25px;
-    top: 50%;
-    transform: translateY(-50%);
-  }
-  &.temperature:before {
-    content: url('./icons/temperature.svg');
-  }
-  &.wind:before {
-    content: url('./icons/wind.svg');
-  }
-  &.pressure:before {
-    content: url('./icons/pressure.svg');
-  }
-  &.visibility:before {
-    content: url('./icons/visibility.svg');
-  }
-  &.humidity:before {
-    content: url('./icons/humidity.svg');
-  }
+}
+img {
+  position: absolute;
+  left: 25px;
+  top: 50%;
+  transform: translateY(-50%);
+  height: 25px;
+  width: 25px;
 }
 </style>
